244dbc784e69606c9b43be22276fe104a9d3a1c7
Benjamin <beschaef@htwg-konstanz.de>
2018-04-15 13:42:37 +0200

    added impl for Index

--- src/memory/paging/table.rs
+++ src/memory/paging/table.rs
@@ -1,6 +1,21 @@
 use memory::paging::entry::*;
 use memory::paging::ENTRY_COUNT;
+use core::ops::{Index, IndexMut};
 
 pub struct Table {
     entries: [Entry; ENTRY_COUNT],
+}
+
+impl Index<usize> for Table {
+    type Output = Entry;
+
+    fn index(&self, index: usize) -> &Entry {
+        &self.entries[index]
+    }
+}
+
+impl IndexMut<usize> for Table {
+    fn index_mut(&mut self, index: usize) -> &mut Entry {
+        &mut self.entries[index]
+    }
 }
