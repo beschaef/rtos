6ecc7dbc740e521dd4c8b3e65a841b42e8969b72
Benjamin <beschaef@htwg-konstanz.de>
2018-04-15 13:41:50 +0200

    added table

--- src/memory/paging/mod.rs
+++ src/memory/paging/mod.rs
@@ -1,4 +1,5 @@
 use memory::PAGE_SIZE; // needed later
+use self::paging::PhysicalAddress;
 
 const ENTRY_COUNT: usize = 512;
 
@@ -7,5 +8,12 @@ pub type VirtualAddress = usize;
 
 pub struct Page {
     number: usize,
+}
+
+impl Page {
+
+    fn start_address(&self) -> PhysicalAddress {
+        self.number * PAGE_SIZE
+    }
 }
 
