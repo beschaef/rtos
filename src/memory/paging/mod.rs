f484098519ce353f362e6d793b39a66eeeafc394
Benjamin <beschaef@htwg-konstanz.de>
2018-04-15 13:28:35 +0200

    added entry submodule

--- src/memory/paging/mod.rs
+++ src/memory/paging/mod.rs
@@ -1,4 +1,5 @@
 use memory::PAGE_SIZE; // needed later
+use memory::Frame; // needed later
 
 const ENTRY_COUNT: usize = 512;
 
@@ -7,4 +8,16 @@ pub type VirtualAddress = usize;
 
 pub struct Page {
     number: usize,
+}
+
+pub struct Entry(u64);
+
+impl Entry {
+    pub fn is_unused(&self) -> bool {
+        self.0 == 0
+    }
+
+    pub fn set_unused(&mut self) {
+        self.0 = 0;
+    }
 }
