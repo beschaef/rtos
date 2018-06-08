e66ff0b37ffebbf55a3042d48d39a226e21445df
beschaef <beschaef@htwg-konstanz.de>
2018-06-08 08:15:05 +0200

    added idle task

--- src/scheduler/mod.rs
+++ src/scheduler/mod.rs
@@ -11,7 +11,9 @@ use x86_64;
 use x86_64::structures::idt::{ExceptionStackFrame, Idt, PageFaultErrorCode};
 use x86_64::structures::tss::TaskStateSegment;
 use x86_64::VirtualAddress;
+use x86_64::instructions::rdtsc;
 use HEAP_ALLOCATOR;
+use features::get_cpu_freq;
 
 pub struct TaskData {
     cpu_flags: u64,
@@ -43,6 +45,12 @@ pub static mut RUNNING_TASK: Mutex<TaskData> = Mutex::new(TaskData::dummy());
 lazy_static! {
     static ref TASKS: Mutex<Vec<TaskData>> = Mutex::new(vec![]);
     static ref NO_MAIN: u8 = 0;
+    static ref IDLE_TASK: Mutex<TaskData> = Mutex::new(TaskData::new(
+        0,
+        x86_64::VirtualAddress(0),
+        x86_64::VirtualAddress(idle_task as usize),
+        1,
+    ));
 }
 
 pub fn sched_init(memory_controller: &mut MemoryController) {
@@ -71,36 +79,24 @@ pub fn sched_init(memory_controller: &mut MemoryController) {
 pub fn uptime1() {
     let color = Color::LightGreen;
     let mut r = 0;
-    let mut x = 0;
     loop {
-        x = (x + 1) % 10000000;
 
-        //        unsafe {
-        //            let speicher = HEAP_ALLOCATOR.alloc(&format!("{}",r));
-        //            vga_buffer::write_at(speicher, 0, 0 + 7, color);
-        //        }
-        if x == 100 {
-            //<-- nach sched
-
-            r = (r + 1) % 9;
-
-            // <--- vor sched
-
-            let color = Color::LightGreen;
-            match r {
-                0 => vga_buffer::write_at("1", 0, 0 + 7, color),
-                1 => vga_buffer::write_at("2", 0, 0 + 7, color),
-                2 => vga_buffer::write_at("3", 0, 0 + 7, color),
-                3 => vga_buffer::write_at("4", 0, 0 + 7, color),
-                4 => vga_buffer::write_at("5", 0, 0 + 7, color),
-                5 => vga_buffer::write_at("6", 0, 0 + 7, color),
-                6 => vga_buffer::write_at("7", 0, 0 + 7, color),
-                7 => vga_buffer::write_at("8", 0, 0 + 7, color),
-                8 => vga_buffer::write_at("9", 0, 0 + 7, color),
-                9 => vga_buffer::write_at("0", 0, 0 + 7, color),
-                _ => vga_buffer::write_at("0", 0, 0 + 7, color),
-            }
+        r = (r + 1) % 9;
+        let color = Color::LightGreen;
+        match r {
+            0 => vga_buffer::write_at("1", 0, 0 + 7, color),
+            1 => vga_buffer::write_at("2", 0, 0 + 7, color),
+            2 => vga_buffer::write_at("3", 0, 0 + 7, color),
+            3 => vga_buffer::write_at("4", 0, 0 + 7, color),
+            4 => vga_buffer::write_at("5", 0, 0 + 7, color),
+            5 => vga_buffer::write_at("6", 0, 0 + 7, color),
+            6 => vga_buffer::write_at("7", 0, 0 + 7, color),
+            7 => vga_buffer::write_at("8", 0, 0 + 7, color),
+            8 => vga_buffer::write_at("9", 0, 0 + 7, color),
+            9 => vga_buffer::write_at("0", 0, 0 + 7, color),
+            _ => vga_buffer::write_at("0", 0, 0 + 7, color),
         }
+        msleep(1000);
     }
 }
 pub fn uptime2() {
@@ -108,25 +104,44 @@ pub fn uptime2() {
     let mut l = -1;
     let mut x = 0;
     loop {
-        x = (x + 1) % 10_000_000;
-        if x == 1000 {
-            l = (l + 1) % 9;
-            let color = Color::LightGreen;
-            match l {
-                0 => vga_buffer::write_at("1", 2, 0 + 7, color),
-                1 => vga_buffer::write_at("2", 2, 0 + 7, color),
-                2 => vga_buffer::write_at("3", 2, 0 + 7, color),
-                3 => vga_buffer::write_at("4", 2, 0 + 7, color),
-                4 => vga_buffer::write_at("5", 2, 0 + 7, color),
-                5 => vga_buffer::write_at("6", 2, 0 + 7, color),
-                6 => vga_buffer::write_at("7", 2, 0 + 7, color),
-                7 => vga_buffer::write_at("8", 2, 0 + 7, color),
-                8 => vga_buffer::write_at("9", 2, 0 + 7, color),
-                9 => vga_buffer::write_at("0", 2, 0 + 7, color),
-                _ => vga_buffer::write_at("0", 2, 0 + 7, color),
-            }
+
+        l = (l + 1) % 9;
+        let color = Color::LightGreen;
+        match l {
+            0 => vga_buffer::write_at("1", 2, 0 + 7, color),
+            1 => vga_buffer::write_at("2", 2, 0 + 7, color),
+            2 => vga_buffer::write_at("3", 2, 0 + 7, color),
+            3 => vga_buffer::write_at("4", 2, 0 + 7, color),
+            4 => vga_buffer::write_at("5", 2, 0 + 7, color),
+            5 => vga_buffer::write_at("6", 2, 0 + 7, color),
+            6 => vga_buffer::write_at("7", 2, 0 + 7, color),
+            7 => vga_buffer::write_at("8", 2, 0 + 7, color),
+            8 => vga_buffer::write_at("9", 2, 0 + 7, color),
+            9 => vga_buffer::write_at("0", 2, 0 + 7, color),
+            _ => vga_buffer::write_at("0", 2, 0 + 7, color),
+        }
+        msleep(1000);
+    }
+}
+
+fn idle_task(){
+    loop{
+        unsafe{asm!("pause":::: "intel", "volatile");}
+    }
+}
+
+
+pub fn msleep(ms: u64) {
+    let one_sec = get_cpu_freq();
+    let time = one_sec * (1000 / ms) + rdtsc();
+    loop {
+        if time > rdtsc() {
+        }
+        else {
+            break;
         }
     }
+
 }
 
 pub fn schedule(f: &mut ExceptionStackFrame) {
