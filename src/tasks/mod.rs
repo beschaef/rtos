7e0eee0e4b0d64ec5189eab7cf86488f87103f2d
daschwin <daniel.schwind@htwg-konstanz.de>
2018-06-29 13:05:17 +0200

    documentation added

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -9,45 +9,69 @@ use x86_64::VirtualAddress;
 
 static mut PID_COUNTER: usize = 0;
 
+
+/// set the size of the playing field
 const BOARD_WIDTH: u8 = 20; //20
 const BOARD_HEIGHT: u8 = 17; //17
+
+/// set the position of the playing field (distance to left/top corner)
 const ROW_OFFSET: u8 = 2;
 const COL_OFFSET: u8 = 50;
 
+
 lazy_static! {
+/// the actual falling piece
     pub static ref PIECE: Mutex<Piece> = Mutex::new(Piece {
+        //the previous position of the piece
         oldx: (BOARD_WIDTH / 2) as i8,
         posx: (BOARD_WIDTH / 2) as i8,
+        //the current position
         oldy: 0,
         posy: 0,
+        //the color
         color: Color::Green,
+        //the previous shape of the piece
         oldshape: vec![vec![0]],
+        //the current shape
         shape: vec![vec![0]],
     });
+/// global board value, contains the occupied cells
     pub static ref BOARD: Mutex<Board> = Mutex::new(Board {
         cells: [[None; BOARD_WIDTH as usize]; BOARD_HEIGHT as usize],
     });
 }
 
+
 pub struct Piece {
+    ///the Color of the specific piece
     color: Color,
+    ///the current position of the piece in x-direction
     posx: i8,
+    ///the current position of the piece in y-direction
     posy: i8,
+    ///the previous position of the piece in x-direction
     oldx: i8,
+    ///the previous position of the piece in y-direction
     oldy: i8,
+    ///the previous shape of the piece
     oldshape: Vec<Vec<u8>>,
+    ///the current shape of the piece
     shape: Vec<Vec<u8>>,
 }
 
 impl Piece {
+    /// change the current piece to a random kind of piece
+    /// and put it at the top of the playing field
     pub fn new_random_piece(&mut self) {
         self.oldx = (BOARD_WIDTH / 2) as i8;
         self.posx = (BOARD_WIDTH / 2) as i8;
         self.oldy = 0;
         self.posy = 0;
-        //generate random numer between 0 and 6
+
+        // generate random number between 0 and 6
         let i = rdtsc() % 7;
 
+        // different pice shapes
         match i {
             0 => {
                 self.color = Color::Green;
@@ -94,11 +118,14 @@ impl Piece {
                     vec![0, 0, 0, 0],
                 ];
             }
-            _ => println!("something else"),
+            _ => println!("error creating new random piece"),
         }
     }
 
+
+    /// prints the current piece
     pub fn print_piece(&mut self) {
+        // delet the previos printed piece
         self.each_old_point(&mut |row, col| {
             let oldx = self.oldx + col;
             let oldy = self.oldy + row;
@@ -111,6 +138,7 @@ impl Piece {
             );
         });
 
+        // prints the current piece
         self.each_point(&mut |row, col| {
             let posx = self.posx + col;
             let posy = self.posy + row;
@@ -124,7 +152,18 @@ impl Piece {
         });
     }
 
+    /// Check if the piece can move in the given direction.
+    ///
+    /// # Arguments
+    ///   * `x` - moves the piece in to the side. Positive values for right shift, negative values for left shift.
+    ///   * `y` - moves the piece down. Shouldn't be a negative value.
+    ///
+    /// # Return
+    ///  * `false` - if there would be a crash
+    ///  * `true` -  if there wouldn't be a crash. Also moves the current piece.
+    ///
     pub fn move_piece(&mut self, x: i8, y: i8) -> bool {
+        // clone the piece
         let mut new_piece = Piece {
             oldx: self.posx,
             posx: self.posx + x,
@@ -140,9 +179,11 @@ impl Piece {
             new_piece.oldshape.push(row.clone());
         }
 
+        // check if there would be an collision
         if new_piece.collision_test() {
             false
         } else {
+            // move the piece
             self.oldx = self.posx;
             self.posx = self.posx + x;
             self.oldy = self.posy;
@@ -159,9 +200,11 @@ impl Piece {
         }
     }
 
+    /// rotate the currnet piece counterclockwise
     pub fn rotate(&mut self) {
         let size = self.shape.len();
 
+        ///lone piece
         let mut new_piece = Piece {
             oldx: self.posx,
             posx: self.posx,
@@ -178,6 +221,7 @@ impl Piece {
             new_piece.oldshape.push(row.clone());
         }
 
+        //rotate the "clone"
         for row in 0..size / 2 {
             for col in row..(size - row - 1) {
                 let t = new_piece.shape[row][col];
@@ -191,6 +235,9 @@ impl Piece {
             }
         }
 
+        // check if rotation would crash
+        // if it would crash ignore
+        // else rotate the current piece
         if !new_piece.collision_test() {
             self.oldx = self.posx;
             self.oldy = self.posy;
@@ -214,6 +261,14 @@ impl Piece {
         }
     }
 
+
+    /// Check if the piece would crash against the boarders of the playing field or against an occupied cell.
+    ///
+    /// # Return
+    ///
+    ///  * `true` - if there would be a crash.
+    ///  * `false` -  if there wouldn't be a crash.
+    ///
     pub fn collision_test(&mut self) -> bool {
         let mut found = false;
         self.each_point(&mut |row, col| {
@@ -231,6 +286,7 @@ impl Piece {
         found
     }
 
+    /// add the faling piece to the occupied cells
     pub fn lock_piece(&mut self) {
         self.each_point(&mut |row, col| {
             let x = self.posx + col;
@@ -239,16 +295,16 @@ impl Piece {
         });
     }
 
-    fn each_old_point(&self, callback: &mut FnMut(i8, i8)) {
-        let piece_width = self.oldshape.len() as i8;
-        for row in 0..piece_width {
-            for col in 0..piece_width {
-                if self.oldshape[row as usize][col as usize] != 0 {
-                    callback(row, col);
-                }
-            }
-        }
-    }
+    /// Move the current piece one step down.
+    /// If this is not possible lock the piece and
+    /// create a new random piece at the top of the field
+    /// if this is not possible as well return false
+    ///
+    /// # Return
+    ///
+    ///  * `true` - if the piece is moved down or a new piece could be created.
+    ///  * `false` -  if the the piece couldn't move down and no new piece could be created -> Game over.
+    ///
     pub fn advance_game(&mut self) -> bool {
         if !self.move_piece(0, 1) {
             self.lock_piece();
@@ -260,11 +316,25 @@ impl Piece {
         }
         true
     }
+
+    ///returns each occupied cell of the shape
     fn each_point(&self, callback: &mut FnMut(i8, i8)) {
         let piece_width = self.shape.len() as i8;
         for row in 0..piece_width {
             for col in 0..piece_width {
                 if self.shape[row as usize][col as usize] != 0 {
+                    callback(row, col);
+                }
+            }
+        }
+    }
+
+    ///return each occupied cell of the previous shape
+    fn each_old_point(&self, callback: &mut FnMut(i8, i8)) {
+        let piece_width = self.oldshape.len() as i8;
+        for row in 0..piece_width {
+            for col in 0..piece_width {
+                if self.oldshape[row as usize][col as usize] != 0 {
                     callback(row, col);
                 }
             }
@@ -277,6 +347,8 @@ pub struct Board {
 }
 
 impl Board {
+
+    ///prints the boarders of the playing field
     pub fn render_board(&self) {
         for y in 0..BOARD_HEIGHT {
             vga_buffer::write_at_background(
@@ -302,10 +374,10 @@ impl Board {
                 Color::Red,
                 Color::Red,
             );
-            //vga_buffer::write_at("-", ROW_OFFSET - 1, COL_OFFSET + x, Color::Red);
         }
     }
 
+    /// clears each line that is filled completely
     pub fn clear_lines(&mut self) {
         for row_to_check in (0..BOARD_HEIGHT as usize).rev() {
             while !self.cells[row_to_check].iter().any(|x| *x == None) {
@@ -313,6 +385,7 @@ impl Board {
                 for row in (1..row_to_check + 1).rev() {
                     self.cells[row] = self.cells[row - 1];
                     for col in 0..BOARD_WIDTH as usize {
+                        //transfer the line above to the current
                         if self.cells[row][col] != None {
                             vga_buffer::write_at_background(
                                 "#",
@@ -330,6 +403,7 @@ impl Board {
                                 Color::Black,
                             );
                         }
+                        // clear the line above
                         vga_buffer::write_at_background(
                             " ",
                             ROW_OFFSET + row as u8 - 1,
@@ -530,18 +604,26 @@ pub fn tetris() {
     msleep(1000);
     trace_info!();
     let mut gameover = false;
-    PIECE.lock().new_random_piece();
-    BOARD.lock().render_board();
-    PIECE.lock().print_piece();
 
+    // print boarders of the playing field
+    BOARD.lock().render_board();
+
+    // creat first random piece
+    PIECE.lock().new_random_piece();
+
+    // while the game isnt over
     while !gameover {
         PIECE.lock().print_piece();
+        // new piece every second
         msleep(1000);
+
+        //gameover if the game couldn't be continued
         if !PIECE.lock().advance_game() {
             gameover = true;
             msleep(1000);
         }
     }
+    // end the task
     finish_task();
 }
 
